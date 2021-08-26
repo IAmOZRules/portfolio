c3607b07b0adacdf0972f9aec475ab3ed0412605
IAmOZRules <shreyaansnahata2001@gmail.com>
2021-08-26 21:33:05 +0530

    fixed overflow and changed scroll button

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
-import "./Footer.css";
 import ScrollToTop from "react-scroll-to-top";
+
+import "./Footer.css";
 
 export default function Footer(props) {
   return <div className="footer-div">
-    <ScrollToTop smooth />
+    <ScrollToTop smooth
+      color={props.theme.name === "dark" ? "black" : "white"}
+      style={{
+        backgroundColor: `${props.theme.name === "dark" ? null : "black"}`,
+        padding: '10px',
+     }}
+    />
   </div>;
 }
